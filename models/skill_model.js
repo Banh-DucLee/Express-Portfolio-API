1977333b9664f519f7d429c08ea4bd9648543f85
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-03 12:51:00 +0200

    Remove hexColor from skill model

--- models/skill_model.js
+++ models/skill_model.js
@@ -4,7 +4,6 @@ const skillSchema = mongoose.Schema({
     name: { type: String, required: true },
     imageUrl: { type: String, required: true },
     category: { type: String, required: true, enum: ['language', 'framework', 'tool'] },
-    hexColor: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Skill', skillSchema);
