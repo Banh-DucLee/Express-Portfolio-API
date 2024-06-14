180bd182e18975c0d391076e7943884cabaaa777
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-14 18:31:06 +0200

    Remove Social attribut

--- models/about_model.js
+++ models/about_model.js
@@ -4,8 +4,7 @@ const Social = require('./social_model');
 const aboutSchema = mongoose.Schema({
     text: { type: String, required: true },
     textEnglish: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    socials: [{ type: mongoose.Schema.Types.ObjectId, ref: Social, required: true }]
+    imageUrl: { type: String, required: true }
 });
 
 module.exports = mongoose.model('About', aboutSchema);
